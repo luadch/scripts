3f09d2f2d0723624349ad80db848460668191fc2
pulsar <pulsar-de@users.noreply.github.com>
2016-01-18 18:19:31 +0100

    etc_NewPasswords_v0.4 (multi3)

---
+++ src/etc_NewPasswords_v0.4 (multi3)/lang/etc_NewPasswords.lang.sv
@@ -0,0 +1,48 @@
+﻿return {
+
+    help_title = "etc_NewPasswords.lua",
+    help_usage = "[+!#]newpw true|false",
+    help_desc = "Skapar ett nytt lösenord för användaren vid inloggningen",
+    msg_denied = "Du har inte behörighet att använda detta kommando.",
+    msg_usage = "Användning: [+!#]newpw true|false",
+    msg_disconnect = "Du har kopplats från eftersom du har fått ett nytt lösenord. Lägg till ditt nya lösenord till dina favorithubbar och logga in igen.",
+    msg_report = "Följande användare har fått ett nytt automatiskt slumpmässigt lösenord och har nu blivit frånkopplad: %s",
+    msg_target = [[
+
+
+=== NYTT LÖSENORD TILL DIG =====================================
+
+     Hej %s, du har fått ett nytt lösenord. Använd det att logga in med från och med nu!
+     Ditt nya lösenord är:  %s
+
+     Du kommer att kopplas från nu!
+
+===================================== NYTT LÖSENORD TILL DIG ===
+  ]],
+
+    msg_users_true = [[
+
+
+=== NYA LÖSENORD =====================================
+
+Användare som bytat lösenord:
+
+%s
+===================================== NYA LÖSENORD ===
+  ]],
+
+    msg_users_false = [[
+
+
+=== NYA LÖSENORD =====================================
+
+Användare som inte bytat lösenord:
+
+%s
+===================================== NYA LÖSENORD ===
+  ]],
+
+    ucmd_menu_ct1_1 = { "Hubb", "etc", "NyttLösenord", "visa användare med nya lösenord" },
+    ucmd_menu_ct1_2 = { "Hubb", "etc", "NyttLösenord", "visa användare med gamla lösenord" },
+
+}
